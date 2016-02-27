3d7d709fb892280c547dbb1cb118553b74a44591
Mike Orozco <mike.orozco94+gitlab@gmail.com>
2016-02-27 21:01:46 +0000

    Initial Version

---
+++ lf-code-expander.raw.js
@@ -0,0 +1,20 @@
+(function () {    
+    var i, expanderDiv;
+
+    for (i = 0; i < document.querySelectorAll('div.codeblock').length; ++i) {
+        expanderDiv = document.createElement('div');
+        expanderDiv.className = "codeexpander-" + i;
+        expanderDiv.setAttribute('style', 'text-align:center;width:100%;display:block;position:absolute;bottom:0;left: 0;background-image: linear-gradient(to bottom, #252525, #252525);padding: 0;margin: 0;cursor: pointer;');
+        expanderDiv.innerHTML = '<p style="color: #38b8d4;font-weight: bold;font-size: 18px;">Click To Expand</p>';
+
+        document.querySelectorAll('div.codeblock')[i].appendChild(expanderDiv);
+        document.querySelectorAll('div.codeblock')[i].style.position = 'relative';
+    }
+
+    for (i = 0; i < document.querySelectorAll("div[class^='codeexpander-']").length; ++i) {
+        document.querySelectorAll("div[class^='codeexpander-']")[i].addEventListener('click', function () {
+            this.parentNode.querySelector('div.body > code').style.maxHeight = 'none';
+            this.style.display = 'none';
+        });
+    }
+})();
