711ba21f97772deba293d7caca00cd42ff7b1279
Mike Orozco <mike.orozco94+gitlab@gmail.com>
2016-02-27 21:00:40 +0000

    Initial Version

---
+++ lf-code-expander.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           LeakForums Code Expander
+// @version        1.0.0
+// @description    Allow expanding of code blocks
+// @downloadURL    https://gitlab.com/notmike101/leakforums-code-expander/raw/master/lf-code-expander.user.js
+// @updateURL      https://gitlab.com/notmike101/leakforums-code-expander/raw/master/lf-code-expander.meta.js
+//
+// @author         Mike Orozco / IRDeNial / R00D
+// @namespace      leakforums/expandcodeblocks
+//
+// @include        *://leakforums.net/thread-*
+// @include        *://www.leakforums.net/thread-*
+// @include        *://*.leakforums.net/thread-*
+//
+// @grant          metadata
+// ==/UserScript==
