d6d3918ef444ed5a85aa5fc71717189e8385fec4
Mike Orozco <mike.orozco94+gitlab@gmail.com>
2016-03-01 14:24:34 -0800

    Match quote styling
    Just to add a bit of consistency

--- lf-code-expander.user.js
+++ lf-code-expander.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           LeakForums Code Expander
-// @version        1.1.1
+// @version        1.1.2
 // @description    Allow expanding of code blocks
 // @downloadURL    https://gitlab.com/notmike101/leakforums-code-expander/raw/master/lf-code-expander.user.js
 // @updateURL      https://gitlab.com/notmike101/leakforums-code-expander/raw/master/lf-code-expander.meta.js
@@ -15,4 +15,4 @@
 // @grant          metadata
 // ==/UserScript==
 
-(function () { function c(a) { return a.scrollHeight > a.clientHeight || a.scrollWidth > a.clientWidth; } var a, b; for (a = 0; a < document.querySelectorAll("div.codeblock").length; ++a)c(document.querySelectorAll("div.codeblock")[a].getElementsByTagName("code")[0]) && (b = document.createElement("div"), b.className = "codeexpander-" + a, b.setAttribute("style", "text-align:center;width:100%;display:block;position:absolute;bottom:0;left: 0;background-image: linear-gradient(to bottom, #252525, #252525);padding: 0;margin: 0;cursor: pointer;"), b.innerHTML = '<p style="color: ' + window.bg_color + ';font-weight: bold;font-size: 18px;padding:5px 0;">Click To Expand</p>', document.querySelectorAll("div.codeblock")[a].appendChild(b), document.querySelectorAll("div.codeblock")[a].style.position = "relative"); for (a = 0; a < document.querySelectorAll("div[class^='codeexpander-']").length; ++a)document.querySelectorAll("div[class^='codeexpander-']")[a].addEventListener("click", function () { this.parentNode.querySelector("div.body > code").style.maxHeight = "none"; this.style.display = "none"; }); })();
+(function () { function c(a) { return a.scrollHeight > a.clientHeight || a.scrollWidth > a.clientWidth; } var a, b; for (a = 0; a < document.querySelectorAll("div.codeblock").length; ++a)c(document.querySelectorAll("div.codeblock")[a].getElementsByTagName("code")[0]) && (b = document.createElement("div"), b.className = "codeexpander-" + a, b.setAttribute("style", "text-align:center;width:100%;display:block;position:absolute;bottom:0;left: 0;background-image: linear-gradient(to bottom, #252525, #252525);padding: 0;margin: 0;cursor: pointer;"), b.innerHTML = '<p style="color: #e24f44;font-size: 14px;padding:5px 0;">Click To Expand</p>', document.querySelectorAll("div.codeblock")[a].appendChild(b), document.querySelectorAll("div.codeblock")[a].style.position = "relative"); for (a = 0; a < document.querySelectorAll("div[class^='codeexpander-']").length; ++a)document.querySelectorAll("div[class^='codeexpander-']")[a].addEventListener("click", function () { this.parentNode.querySelector("div.body > code").style.maxHeight = "none"; this.style.display = "none"; }); })();
