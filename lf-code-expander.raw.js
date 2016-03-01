0c8e478d4d770b1e9be029ecd2b470b6887e474c
Mike Orozco <mike.orozco94+gitlab@gmail.com>
2016-03-01 21:38:34 +0000

    Update to only apply to elements that have an overflow attribute

--- lf-code-expander.raw.js
+++ lf-code-expander.raw.js
@@ -1,14 +1,20 @@
 (function () {    
     var i, expanderDiv;
 
-    for (i = 0; i < document.querySelectorAll('div.codeblock').length; ++i) {
-        expanderDiv = document.createElement('div');
-        expanderDiv.className = "codeexpander-" + i;
-        expanderDiv.setAttribute('style', 'text-align:center;width:100%;display:block;position:absolute;bottom:0;left: 0;background-image: linear-gradient(to bottom, #252525, #252525);padding: 0;margin: 0;cursor: pointer;');
-        expanderDiv.innerHTML = '<p style="color: #38b8d4;font-weight: bold;font-size: 18px;">Click To Expand</p>';
+    function isOverflowed(element) {
+        return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
+    }
 
-        document.querySelectorAll('div.codeblock')[i].appendChild(expanderDiv);
-        document.querySelectorAll('div.codeblock')[i].style.position = 'relative';
+    for (i = 0; i < document.querySelectorAll('div.codeblock').length; ++i) {
+        if (isOverflowed(document.querySelectorAll('div.codeblock')[i].getElementsByTagName("code")[0])) {
+            expanderDiv = document.createElement('div');
+            expanderDiv.className = "codeexpander-" + i;
+            expanderDiv.setAttribute('style', 'text-align:center;width:100%;display:block;position:absolute;bottom:0;left: 0;background-image: linear-gradient(to bottom, #252525, #252525);padding: 0;margin: 0;cursor: pointer;');
+            expanderDiv.innerHTML = '<p style="color: #38b8d4;font-weight: bold;font-size: 18px;">Click To Expand</p>';
+
+            document.querySelectorAll('div.codeblock')[i].appendChild(expanderDiv);
+            document.querySelectorAll('div.codeblock')[i].style.position = 'relative';
+        }
     }
 
     for (i = 0; i < document.querySelectorAll("div[class^='codeexpander-']").length; ++i) {
